fd59f97f4a40959ddcb9b55f16bb90e0a37200d6
alberto <albe.carde@gmail.com>
2018-10-15 23:30:53 +0100

    Issue #5: moved generic functions in Strategy parent class

--- scripts/Strategies/SimpleMACD.py
+++ scripts/Strategies/SimpleMACD.py
@@ -4,7 +4,6 @@ import pandas as pd
 import time
 import sys
 import traceback
-from random import shuffle
 
 from .Strategy import Strategy
 from Utils import *
@@ -21,62 +20,8 @@ class SimpleMACD(Strategy):
         self.controlledRisk = config['ig_interface']['controlled_risk']
         self.timeout = 1
 
-    # TODO make as generic as possible and move in Strategy class
-    def spin(self, broker, epic_list):
-        logging.info("Strategy started to spin.")
-        try:
-            # Fetch open positions and process them first
-            positionMap = broker.get_open_positions()
-            logging.info("Processing open positions: {}".format(positionMap))
-            for key, dealSize in positionMap.items():
-                epic = positionMap[key].split('-')[0]
-                self.process_epic(broker, epic)
 
-            # Start processing all the company in the epic list
-            logging.info("Started processing epic list of length: {}".format(len(epic_list)))
-            if len(epic_list) < 1:
-                logging.warn("Epic list is empty!")
-            else:
-                shuffle(epic_list)
-                for epic in epic_list:
-                    try:
-                        self.process_epic(broker, epic)
-                    except Exception as e:
-                        logging.warn(e)
-                        logging.warn(traceback.format_exc())
-                        logging.warn(sys.exc_info()[0])
-                        time.sleep(self.timeout)
-                        continue
-
-            # Define timeout until next iteration of strategy
-            strategyInteval = 3600 # 1 hour in seconds
-            if self.interval == 'HOUR_4':
-                strategyInterval = 60 * 60 * 4
-            logging.info("Epics analysis complete. Wait for {} seconds".format(strategyInterval))
-            time.sleep(strategyInterval)
-        except Exception as e:
-                logging.warn(e)
-                logging.warn(traceback.format_exc())
-                logging.warn(sys.exc_info()[0])
-                logging.warn("Something fucked up.")
-                time.sleep(self.timeout)
-
-    # TODO make as generic as possible and move in Strategy class
-    def process_epic(self, broker, epic):
-        # Process the epic and find if we want to trade
-        trade, limit, stop = self.find_trade_signal(broker, epic)
-
-        # In case of no trade return
-        if trade is TradeDirection.NONE:
-            time.sleep(self.timeout)
-            return
-        else:
-            # Perform safety check for trade action
-            if self.safe_to_trade(broker, epic, trade):
-                broker.trade(epic, trade.name, limit, stop)
-                time.sleep(self.timeout)
-
-    # TODO This should be the only function in this class, possibly split in more smaller ones
+    # TODO  possibly split in more smaller ones
     def find_trade_signal(self, broker, epic_id):
         tradeDirection = TradeDirection.NONE
         limit = None
@@ -88,7 +33,8 @@ class SimpleMACD(Strategy):
 
         # Safety checks before processing the epic
         if (prices is None or 'prices' not in prices
-            or market is None or 'markets' in market):
+            or market is None or 'markets' in market
+            or market['snapshot']['bid'] is None):
             logging.warn('Strategy can`t process {}'.format(epic_id))
             return TradeDirection.NONE, None, None
 
@@ -134,17 +80,3 @@ class SimpleMACD(Strategy):
             logging.info("SimpleMACD says: {} {}".format(tradeDirection, epic_id))
 
         return tradeDirection, limit, stop
-
-    # TODO make as generic as possible and move in Strategy class
-    def safe_to_trade(self, broker, epic, trade):
-        # Check if the account has enough cash available to open new positions
-        balance, deposit = broker.get_account_balances()
-        percent_used = percentage(deposit, balance)
-        if percent_used > self.max_account_usable:
-            logging.warn("Will not trade, {}% of account balance is used."
-                            .format(str(percent_used)))
-            return False
-        else:
-            logging.info("Ok to trade, {}% of account is used"
-                            .format(str(percent_used)))
-            return True
--- scripts/Strategies/Strategy.py
+++ scripts/Strategies/Strategy.py
@@ -1,4 +1,10 @@
 import logging
+import time
+import sys
+import traceback
+from random import shuffle
+
+from Utils import *
 
 class Strategy:
     def __init__(self, config):
@@ -6,7 +12,72 @@ class Strategy:
         self.order_size = config['ig_interface']['order_size']
         self.max_account_usable = config['general']['max_account_usable']
         self.read_configuration(config)
-        pass
 
-    def spin(self, broker, epic_ids):
-        logging.error("Strategy not defined!")
+
+    def spin(self, broker, epic_list):
+        logging.info("Strategy started to spin.")
+        try:
+            # Fetch open positions and process them first
+            positionMap = broker.get_open_positions()
+            logging.info("Processing open positions: {}".format(positionMap))
+            for key, dealSize in positionMap.items():
+                epic = positionMap[key].split('-')[0]
+                self.process_epic(broker, epic)
+
+            # Start processing all the company in the epic list
+            logging.info("Started processing epic list of length: {}".format(len(epic_list)))
+            if len(epic_list) < 1:
+                logging.warn("Epic list is empty!")
+            else:
+                shuffle(epic_list)
+                for epic in epic_list:
+                    try:
+                        self.process_epic(broker, epic)
+                    except Exception as e:
+                        logging.warn(e)
+                        logging.warn(traceback.format_exc())
+                        logging.warn(sys.exc_info()[0])
+                        time.sleep(self.timeout)
+                        continue
+
+            # Define timeout until next iteration of strategy
+            strategyInteval = 3600 # 1 hour in seconds
+            if self.interval == 'HOUR_4':
+                strategyInterval = 60 * 60 * 4
+            logging.info("Epics analysis complete. Wait for {} seconds".format(strategyInterval))
+            time.sleep(strategyInterval)
+        except Exception as e:
+                logging.warn(e)
+                logging.warn(traceback.format_exc())
+                logging.warn(sys.exc_info()[0])
+                logging.warn("Something fucked up.")
+                time.sleep(self.timeout)
+
+
+    def process_epic(self, broker, epic):
+        # Process the epic and find if we want to trade
+        trade, limit, stop = self.find_trade_signal(broker, epic)
+
+        # In case of no trade return
+        if trade is TradeDirection.NONE:
+            time.sleep(self.timeout)
+            return
+        else:
+            # Perform safety check for trade action
+            if self.safe_to_trade(broker, epic, trade):
+                broker.trade(epic, trade.name, limit, stop)
+                time.sleep(self.timeout)
+
+
+    def safe_to_trade(self, broker, epic, trade):
+        # Check if the account has enough cash available to open new positions
+        balance, deposit = broker.get_account_balances()
+        percent_used = percentage(deposit, balance)
+        if percent_used > self.max_account_usable:
+            logging.warn("Will not trade, {}% of account balance is used."
+                            .format(str(percent_used)))
+            return False
+        else:
+            logging.info("Ok to trade, {}% of account is used"
+                            .format(str(percent_used)))
+            return True
