751f7cf7e7c36bb64c91b54a57ba3c224f17fe29
alberto <albe.carde@gmail.com>
2018-10-24 23:01:04 +0100

    Changed the installation to reuse the cron script

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ echo Installing TradingBot in $INSTALL_DIR...
 # Create installation folder
 mkdir -p $INSTALL_DIR
 mkdir -p $INSTALL_DIR/data
-# Copy config files
+# Copy files
 cp -ar config/ $INSTALL_DIR/config
-cp -ar cron/ $INSTALL_DIR/cron
-cp -ar scripts $INSTALL_DIR/scripts/
+cp -ar cron/trading_bot_ctl $INSTALL_DIR
+cp -ar scripts/ $INSTALL_DIR/scripts
 # Make the main script executable
-chmod 755 $INSTALL_DIR/scripts/trading_bot.py
+chmod 755 $INSTALL_DIR/trading_bot_ctl
 
 echo TradingBot installed.
+echo Follow setup instructions at: https://github.com/ilcardella/TradingBot
+echo Remember to configure TradingBot editing the config.json file too!
--- scripts/trading_bot.py
+++ scripts/trading_bot.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 ###############################################################################
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
