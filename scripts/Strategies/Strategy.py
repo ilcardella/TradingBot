e53b202f2bdf2ff395ca09d2ee38349016829ec4
alberto <albe.carde@gmail.com>
2018-11-18 00:36:00 +0000

    When market is closed wait until next market opening and not check every minute

--- scripts/Strategies/Strategy.py
+++ scripts/Strategies/Strategy.py
@@ -1,3 +1,4 @@
+from Utils import Utils, TradeDirection
 import logging
 import time
 import traceback
@@ -8,17 +9,18 @@ import os
 import inspect
 import sys
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+sys.path.insert(0, parentdir)
 
-from Utils import Utils, TradeDirection
 
 class Strategy:
     """
     Generic strategy template to use as a parent class for custom strategies.
     Provide safety checks for new trades and handling of open positions.
     """
+
     def __init__(self, config, services):
         self.positions = {}
         self.broker = services['broker']
@@ -27,8 +29,9 @@ class Strategy:
         self.time_zone = config['general']['time_zone']
         self.order_size = config['ig_interface']['order_size']
         self.max_account_usable = config['general']['max_account_usable']
-        self.spin_interval = config['strategies']['spin_interval'] # This can be overwritten in children class
-        self.timeout = 1 # Delay between each find_trade_signal() call
+        # This can be overwritten in children class
+        self.spin_interval = config['strategies']['spin_interval']
+        self.timeout = 1  # Delay between each find_trade_signal() call
 
         # This must be the last operation of this function to override possible values in children class
         self.read_configuration(config)
@@ -59,7 +62,6 @@ class Strategy:
 ##############################################################
 ##############################################################
 
-
     def start(self, epic_list):
         """
         Start the strategy processing the given list of epic ids. Wait until the
@@ -71,10 +73,11 @@ class Strategy:
             if self.isMarketOpen(self.time_zone):
                 self.spin(epic_list)
             else:
-                logging.info("Market is closed! Wait 60 seconds...")
-                time.sleep(60)
+                seconds = Utils.get_seconds_to_market_opening()
+                logging.info(
+                    "Market is closed! Wait for {0:.2f} hours...".format(seconds / 3600))
+                time.sleep(seconds)
                 continue
-
 
     def spin(self, epic_list):
         """
@@ -98,11 +101,13 @@ class Strategy:
             # Check if the account has enough cash available to open new positions
             percent_used = self.get_account_used_perc()
             if percent_used < self.max_account_usable:
-                logging.info("Ok to trade, {}% of account is used".format(str(percent_used)))
+                logging.info(
+                    "Ok to trade, {}% of account is used".format(str(percent_used)))
                 if len(epic_list) < 1:
                     logging.warn("Epic list is empty!")
                 else:
-                    logging.info("Started processing epic list of length: {}".format(len(epic_list)))
+                    logging.info(
+                        "Started processing epic list of length: {}".format(len(epic_list)))
                     shuffle(epic_list)
                     for epic in epic_list:
                         try:
@@ -110,7 +115,8 @@ class Strategy:
                                 # If there has been a trade check again account usage
                                 percent_used = self.get_account_used_perc()
                                 if percent_used > self.max_account_usable:
-                                    logging.warn("Stop trading because {}% of account is used".format(str(percent_used)))
+                                    logging.warn(
+                                        "Stop trading because {}% of account is used".format(str(percent_used)))
                                     break
                             time.sleep(self.timeout)
                         except Exception as e:
@@ -121,19 +127,20 @@ class Strategy:
                             continue
             else:
                 logging.warn("Will not trade, {}% of account balance is used."
-                                .format(str(percent_used)))
+                             .format(str(percent_used)))
 
             # If interval is set to -1 in config file then the strategy should provide its own interval
-            seconds = self.get_seconds_to_next_spin() if self.spin_interval < 0 else self.spin_interval
-            logging.info("Epics analysis complete. Wait for {} seconds".format(seconds))
+            seconds = self.get_seconds_to_next_spin(
+            ) if self.spin_interval < 0 else self.spin_interval
+            logging.info(
+                "Epics analysis complete. Wait for {} seconds".format(seconds))
             time.sleep(seconds)
         except Exception as e:
-                logging.error(e)
-                logging.error(traceback.format_exc())
-                logging.error(sys.exc_info()[0])
-                logging.error("Something fucked up.")
-                time.sleep(self.timeout)
-
+            logging.error(e)
+            logging.error(traceback.format_exc())
+            logging.error(sys.exc_info()[0])
+            logging.error("Something fucked up.")
+            time.sleep(self.timeout)
 
     def process_epic(self, epic):
         """
@@ -151,16 +158,17 @@ class Strategy:
                 for item in self.positions['positions']:
                     # If a same direction trade already exist, don't trade
                     if item['market']['epic'] == epic and trade.name == item['position']['direction']:
-                        logging.warn("There is already an open position for this epic, skip trade")
+                        logging.warn(
+                            "There is already an open position for this epic, skip trade")
                         return False
                     # If a trade in opposite direction exist, close the position
                     elif item['market']['epic'] == epic and trade.name != item['position']['direction']:
                         return self.broker.close_position(item)
                 return self.broker.trade(epic, trade.name, limit, stop)
             else:
-                logging.error("Unable to retrieve open positions! Avoid trading this epic")
+                logging.error(
+                    "Unable to retrieve open positions! Avoid trading this epic")
         return False
-
 
     def get_account_used_perc(self):
         """
@@ -170,9 +178,8 @@ class Strategy:
         """
         balance, deposit = self.broker.get_account_balances()
         if balance is None or deposit is None:
-            return 9999999 # This will block the trading
+            return 9999999  # This will block the trading
         return Utils.percentage(deposit, balance)
-
 
     def isMarketOpen(self, timezone):
         """
