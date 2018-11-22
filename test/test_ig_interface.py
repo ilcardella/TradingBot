101c7e3ac014b9a05c1db28a560ff7bdeaf898ba
alberto <albe.carde@gmail.com>
2018-11-22 20:00:48 +0000

    Added unit test for iginterface

--- test/test_ig_interface.py
+++ test/test_ig_interface.py
@@ -2,12 +2,15 @@ import os
 import sys
 import inspect
 import pytest
+import json
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,'{}/scripts'.format(parentdir))
+sys.path.insert(0, '{}/scripts'.format(parentdir))
 
 from Interfaces.IGInterface import IGInterface
+
 
 @pytest.fixture
 def config():
@@ -15,18 +18,19 @@ def config():
     Returns a dict with config parameter for ig_interface
     """
     return {
-        "ig_interface" : {
-        "order_type": "MARKET",
-        "order_size": 1,
-        "order_expiry": "DFB",
-        "order_currency": "GBP",
-        "order_force_open": True,
-        "use_g_stop": True,
-        "use_demo_account": True,
-        "controlled_risk": True,
-        "paper_trading": False
+        "ig_interface": {
+            "order_type": "MARKET",
+            "order_size": 1,
+            "order_expiry": "DFB",
+            "order_currency": "GBP",
+            "order_force_open": True,
+            "use_g_stop": True,
+            "use_demo_account": True,
+            "controlled_risk": True,
+            "paper_trading": False
         }
     }
+
 
 @pytest.fixture
 def credentials():
@@ -41,12 +45,23 @@ def credentials():
         "av_api_key": "12345"
     }
 
+
 @pytest.fixture
 def ig(config):
     """
     Returns a instance of IGInterface
     """
     return IGInterface(config)
+
+
+def read_json(filepath):
+    # Read mock file
+    try:
+        with open(filepath, 'r') as file:
+            return json.load(file)
+    except IOError:
+        exit()
+
 
 def test_init(ig, config):
     assert ig.orderType == config['ig_interface']['order_type']
@@ -58,15 +73,18 @@ def test_init(ig, config):
     assert ig.useDemo == config['ig_interface']['use_demo_account']
     assert ig.paperTrading == config['ig_interface']['paper_trading']
 
+
 def test_authenticate(ig, credentials, requests_mock):
     # Define mock requests
     mock_headers = {
-            'CST':'mock',
-            'X-SECURITY-TOKEN':'mock'
-            }
-    # TODO create mock json file to load and return instead of text
-    requests_mock.post(ig.apiBaseURL+'/session', text='Success', headers=mock_headers)
-    requests_mock.put(ig.apiBaseURL+'/session', text='Success')
+        'CST': 'mock',
+        'X-SECURITY-TOKEN': 'mock'
+    }
+    data = read_json('test/test_data/mock_ig_login.json')
+    requests_mock.post(ig.apiBaseURL+'/session',
+                       json=data, headers=mock_headers)
+    data = read_json('test/test_data/mock_ig_set_account.json')
+    requests_mock.put(ig.apiBaseURL+'/session', json=data)
 
     # Call function to test
     result = ig.authenticate(credentials)
@@ -76,13 +94,19 @@ def test_authenticate(ig, credentials, requests_mock):
     assert ig.authenticated_headers['X-SECURITY-TOKEN'] == mock_headers['X-SECURITY-TOKEN']
     assert result == True
 
+
 def test_authenticate_fail(ig, credentials, requests_mock):
     # Define mock requests
     mock_headers = {
-            'CST':'mock',
-            'X-SECURITY-TOKEN':'mock'
-            }
-    requests_mock.post(ig.apiBaseURL+'/session', text='Fail', status_code=401, headers=mock_headers)
+        'CST': 'mock',
+        'X-SECURITY-TOKEN': 'mock'
+    }
+    data = {
+        "errorCode": "error.security.invalid-details"
+    }
+    requests_mock.post(ig.apiBaseURL+'/session', text='Fail',
+                       status_code=401, headers=mock_headers)
+    data = read_json('test/test_data/mock_ig_set_account.json')
     requests_mock.put(ig.apiBaseURL+'/session', text='Success')
 
     # Call function to test
@@ -91,26 +115,31 @@ def test_authenticate_fail(ig, credentials, requests_mock):
     # Assert results
     assert result == False
 
+
 def test_set_default_account(ig, credentials, requests_mock):
-    # TODO create mock json file to load and return instead of text
-    requests_mock.put(ig.apiBaseURL+'/session', status_code=200, text='Success')
+    data = read_json('test/test_data/mock_ig_set_account.json')
+    requests_mock.put(ig.apiBaseURL+'/session', status_code=200, json=data)
 
     result = ig.set_default_account(credentials['account_id'])
 
     assert result == True
 
+
 def test_set_default_account_fail(ig, credentials, requests_mock):
-    requests_mock.put(ig.apiBaseURL+'/session', status_code=403, text='Success')
+    requests_mock.put(ig.apiBaseURL+'/session',
+                      status_code=403, text='Success')
 
     result = ig.set_default_account(credentials['account_id'])
 
     assert result == False
 
+
 def test_get_account_balance(ig, requests_mock):
-    # TODO create mock json file to load and return instead of text
-    requests_mock.put(ig.apiBaseURL+'/session', status_code=200, text='Success')
+    data = read_json('test/test_data/mock_ig_account_details.json')
+    requests_mock.put(ig.apiBaseURL+'/session', status_code=200, json=data)
     balance, deposit = ig.get_account_balance()
 
     assert balance is not None
     assert deposit is not None
-    # TODO assert values are == in mock json file
+    assert balace == 16093.12
+    assert deposit == 0.0
