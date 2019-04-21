93a17bd9ed1f28baf0d1314caedea921654cfbbc
alberto <albe.carde@gmail.com>
2019-04-21 14:43:59 +0100

    Updated version in documentation

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Alberto Cardellini'
 author = 'Alberto Cardellini'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
