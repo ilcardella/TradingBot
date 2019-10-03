217e6346510c19a3ed7ac6475cae0d8522a76f11
Alberto Cardellini <albe.carde@gmail.com>
2019-10-04 00:12:46 +0100

    config file is injected in TradingBot init

--- test/test_trading_bot.py
+++ test/test_trading_bot.py
@@ -10,15 +10,17 @@ sys.path.insert(0, "{}/src".format(parentdir))
 
 from TradingBot import TradingBot
 from Components.TimeProvider import TimeProvider
+from common.MockRequests import ig_request_login, ig_request_set_account
 
 
-def test_trading_bot_001():
+def test_trading_bot_init(requests_mock):
     """
-    TODO Define configuration of this test (list, watchlist, buy, sell,etc.)
+    Test init
     """
-    # TODO
-    # tb = TradingBot(TimeProvider())
-    # tb2 = TradingBot()
-    # assert tb
-    # assert tb2
-    assert True
+    ig_request_login(requests_mock)
+    ig_request_set_account(requests_mock)
+    config = "test/test_data/config.json"
+    tb = TradingBot(TimeProvider(), config_filepath=config)
+    tb2 = TradingBot(config_filepath=config)
+    assert tb
+    assert tb2
