8a756661828c0c262fb723ac8f54f09e12e9e141
Alberto Cardellini <albe.carde@gmail.com>
2020-12-19 12:40:42 +0000

    Added buy condition to simple bollinger bands strategy (#317)
    * strategy uses also last close price
    
    * Added buy condition in simple bollinger bands strategy
    
    * poetry update
    
    * removed matplotlib dep

--- tradingbot/strategies/simple_bollinger_bands.py
+++ tradingbot/strategies/simple_bollinger_bands.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 
+# import matplotlib.pyplot as plt
 import pandas
 
 from ..components import Configuration, Interval, TradeDirection, Utils
@@ -36,42 +37,71 @@ class SimpleBollingerBands(Strategy):
 
     def fetch_datapoints(self, market: Market) -> MarketHistory:
         """
-        Fetch historic prices up to 20 day in the past
+        Fetch historic prices
         """
-        return self.broker.get_prices(market, Interval.DAY, self.window)
+        return self.broker.get_prices(market, Interval.DAY, self.window * 2)
 
     def find_trade_signal(
         self, market: Market, datapoints: MarketHistory
     ) -> TradeSignal:
+        # Copy only the required amount of data
         df = datapoints.dataframe[: self.window * 2].copy()
         indexer = pandas.api.indexers.FixedForwardWindowIndexer(window_size=self.window)
-        # 1. Compute the price moving averate
+        # Compute the price moving averate
         df["MA"] = df[MarketHistory.CLOSE_COLUMN].rolling(window=indexer).mean()
-        # 2. Compute the prices standard deviation
+        # Compute the prices standard deviation
         # set .std(ddof=0) for population std instead of sample
         df["STD"] = df[MarketHistory.CLOSE_COLUMN].rolling(window=indexer).std()
-        # 1. Compute upper band
+        # Compute upper band
         df["Upper_Band"] = df["MA"] + (df["STD"] * 2)
-        # 2. Compute lower band
+        # Compute lower band
         df["Lower_Band"] = df["MA"] - (df["STD"] * 2)
-        # 3. Compare the last price with the band boundaries and trigger signals
-        if df[MarketHistory.CLOSE_COLUMN].iloc[1] < df["Lower_Band"].iloc[1]:
-            return self._buy(market.offer, market.bid)
-        # elif df[MarketHistory.CLOSE_COLUMN].iloc[1] > df["Upper_Band"].iloc[1]:
-        #     return self._buy(market.offer, market.bid)
+
+        # self._plot(df)
+
+        # Compare the last price with the band boundaries and trigger signals
+        cross_lower_band_and_back = (
+            df[MarketHistory.CLOSE_COLUMN].iloc[0] > df["Lower_Band"].iloc[0]
+        ) and (df[MarketHistory.CLOSE_COLUMN].iloc[1] <= df["Lower_Band"].iloc[1])
+        stable_below_ma = (
+            df[MarketHistory.CLOSE_COLUMN].iloc[0:5] < df["MA"].iloc[0:5]
+        ).all()
+
+        if any([cross_lower_band_and_back, stable_below_ma]):
+            return self._buy_signal(market)
         return TradeDirection.NONE, None, None
 
-    def _buy(self, offer: float, bid: float) -> TradeSignal:
+    def _buy_signal(self, market: Market) -> TradeSignal:
         direction = TradeDirection.BUY
-        limit = offer + Utils.percentage_of(self.limit_p, offer)
-        stop = bid - Utils.percentage_of(self.stop_p, bid)
+        limit = market.offer + Utils.percentage_of(self.limit_p, market.offer)
+        stop = market.bid - Utils.percentage_of(self.stop_p, market.bid)
         return direction, limit, stop
 
-    def _sell(self, offer: float, bid: float) -> TradeSignal:
+    def _sell_signal(self, market: Market) -> TradeSignal:
         direction = TradeDirection.SELL
-        limit = bid - Utils.percentage_of(self.limit_p, bid)
-        stop = offer + Utils.percentage_of(self.stop_p, offer)
+        limit = market.bid - Utils.percentage_of(self.limit_p, market.bid)
+        stop = market.offer + Utils.percentage_of(self.stop_p, market.offer)
         return direction, limit, stop
+
+    # def _plot(self, dataframe: pandas.DataFrame):
+    #     ax = plt.gca()
+    #     dataframe.plot(
+    #         kind="line",
+    #         x=MarketHistory.DATE_COLUMN,
+    #         y=MarketHistory.CLOSE_COLUMN,
+    #         ax=ax,
+    #         color="blue",
+    #     )
+    #     dataframe.plot(
+    #         kind="line", x=MarketHistory.DATE_COLUMN, y="Lower_Band", ax=ax, color="red"
+    #     )
+    #     dataframe.plot(
+    #         kind="line", x=MarketHistory.DATE_COLUMN, y="Upper_Band", ax=ax, color="red"
+    #     )
+    #     dataframe.plot(
+    #         kind="line", x=MarketHistory.DATE_COLUMN, y="MA", ax=ax, color="green"
+    #     )
+    #     plt.show()
 
     def backtest(
         self, market: Market, start_date: datetime, end_date: datetime
